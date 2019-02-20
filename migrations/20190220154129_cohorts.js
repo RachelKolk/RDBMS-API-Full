0dfdfaad644eb5a14ab42b19a7643171787d50d2
RachelKolk <rach.m.obrien@gmail.com>
2019-02-20 15:50:29 -0500

    knex migration for the cohort schema

---
+++ migrations/20190220154129_cohorts.js
@@ -0,0 +1,16 @@
+//sets up changes that will be applied to the database
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cohorts', function(tbl) {
+        //sets the primary key (id) as an auto-incrementing integer
+        tbl.increments();
+
+        //set the name column as varchar with required text
+        tbl.string('name', 255).notNullable();
+    })
+  
+};
+
+//sets up how we can undo the applied changes
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('cohorts');
+};
