b2f2b8f8cc0afa13ee8e8c705b090086c28311c5
RachelKolk <rach.m.obrien@gmail.com>
2019-02-20 17:00:34 -0500

    get and get by id endpoint requests for cohorts tbale

--- index.js
+++ index.js
@@ -18,6 +18,37 @@ server.use(helmet());
 server.use(express.json());
 
 
+//  <<<<<<<<<<<<<<<<<<<<<<<<  Receive Requests  >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//GET all entries from the api/cohorts table
+server.get('/api/cohorts', async (req, res) => {
+    try {
+        const cohorts = await db('cohorts');
+        res.status(200).json(cohorts);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//GET entries from the api/cohorts table by id
+server.get('/api/cohorts/:id', async (req, res) => {
+    try {
+        const cohort = await db('cohorts')
+        .where({id: req.params.id})
+        .first();
+
+        if (cohort) {
+            res.status(200).json(cohort);
+        } else {
+            res.status(404).json({message: "A cohort with that id does not exist"})
+        }
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\n***<<< Running on ${port} >>>***\n`));
